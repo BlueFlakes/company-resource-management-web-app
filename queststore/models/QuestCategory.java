b66660b68c4e72f8e6ac2a36c1dae99f153f0dfe
jakubskiba <jakubskibakrk@gmail.com>
2017-08-29 19:25:04 +0200

    implement QuestCategory model

---
+++ queststore/models/QuestCategory.java
@@ -0,0 +1,13 @@
+package queststore.models;
+
+public class QuestCategory{
+    private String name;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
