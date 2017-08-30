7981428290bc41ba8674f350a04ecf1faef24043
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-30 11:10:43 +0200

    Implement login() and start() methods in LoginController

--- queststore/controllers/LoginController.java
+++ queststore/controllers/LoginController.java
@@ -1,11 +1,50 @@
+package questore.controllers;
+
+import views.View;
+import models.School;
+import models.User;
+import models.Student;
+import models.Mentor;
+import models.Manager;
+
+
 public class LoginController {
 
+    private School school;
+    private View view = new View();
+
+    public LoginController(School school) {
+        this.school = school;
+    }
+
     public void start() {
+        String username = view.getInput("Please provide your username: ");
+        String password = view.getInput("Please provide your password: ");
+
+        try {
+            User user = login(username, password);
+            runUserController(user);
+        } catch (WrongPasswordException e) {
+            view.print(e.getMessage());
+        }
+
     }
 
-    private void login(String login, String password) {
+    private User login(String login, String password) throws WrongPasswordException {
+        ArrayList<User> users = school.getAllUsers();
+
+        for (User user : users) {
+            String correctLogin = user.getLogin();
+            String correctPassword = user.getPassword();
+
+            if (correctLogin.equals(login) && correctPassword.equals(password)) {
+                return User;
+            }
+        }
+
+        throw WrongPasswordException("Invalid login or password!");
     }
 
-    private runUserController(User user) {
+    private void runUserController(User user) {
     }
 }
