8b1b85f59d6b9c9e1e2471a742f1d550b16aab33
jakubskiba <jakubskibakrk@gmail.com>
2017-08-30 10:58:53 +0200

    fix constructor in manager

--- queststore/models/Manager.java
+++ queststore/models/Manager.java
@@ -1,5 +1,7 @@
 package queststore.models;
 
 public class Manager extends User{
-
+    public Manager(String name, String login, String password, String email) {
+        super(name, login, password, email);
+    }
 }
