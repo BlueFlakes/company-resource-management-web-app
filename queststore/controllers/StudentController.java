ed7bf1e5b0df06cc47ed21ba22a59dc16526372b
jakubskiba <jakubskibakrk@gmail.com>
2017-08-30 20:58:02 +0200

    implement start controller method in student controller

--- queststore/controllers/StudentController.java
+++ queststore/controllers/StudentController.java
@@ -1,11 +1,27 @@
 package queststore.controllers;
 
+import queststore.interfaces.UserController;
 import queststore.models.User;
 import queststore.models.School;
+import queststore.views.UserInterface;
 
 public class StudentController {
+    User user;
+    School school;
+    UserInterface userInterface = new UserInterface();
 
     public void startController(User user, School school) {
+        this.user = user;
+        this.school = school;
+
+        String userChoice = "";
+        while(!userChoice.equals("0")){
+            this.userInterface.printStudentMenu();
+            userChoice = this.userInterface.getInput("Provide options");
+            handleUserRequest(userChoice);
+            this.userInterface.getInput("Press enter to continue...");
+            school.save();
+        }
     }
 
     private void showWallet() {
