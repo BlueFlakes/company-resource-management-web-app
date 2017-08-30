2ae27b99cd0414c95c28093311eaa21354197145
jakubskiba <jakubskibakrk@gmail.com>
2017-08-31 00:36:37 +0200

    implement start controller in user store controller

--- queststore/controllers/StudentStoreController.java
+++ queststore/controllers/StudentStoreController.java
@@ -1,12 +1,31 @@
 package queststore.controllers;
 
 import queststore.interfaces.UserController;
-import queststore.models.School;
 import queststore.models.User;
+import queststore.models.School;
+import queststore.views.UserInterface;
 
 public class StudentStoreController implements UserController {
+    User user;
+    School school;
+    UserInterface userInterface = new UserInterface();
 
     public void startController(User user, School school) {
+        this.user = user;
+        this.school = school;
+
+        String userChoice = "";
+        while(!userChoice.equals("0")){
+            this.userInterface.printStudentStoreMenu();
+            userChoice = this.userInterface.getInput("Provide options");
+            handleUserRequest(userChoice);
+            if(!userChoice.equals("0")){
+                this.userInterface.getInput("Press enter to continue...");
+            }
+            school.save();
+        }
+    }
+
     }
 
     private void buyArtifact() {
