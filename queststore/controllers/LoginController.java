83d92a4ad05ba531f72e6c425e8e6ee8bfc3613c
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-30 09:59:02 +0200

    Update controller UML

---
+++ queststore/controllers/LoginController.java
@@ -0,0 +1,11 @@
+public class LoginController {
+
+    public void start() {
+    }
+
+    private void login(String login, String password) {
+    }
+
+    private runUserController(User user) {
+    }
+}
