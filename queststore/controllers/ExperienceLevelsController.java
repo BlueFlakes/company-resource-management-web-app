022a8e6eff785abf7c2bf7e3873f04e99768111b
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-30 14:40:48 +0200

    Add ExperienceLevelsController mock-up

---
+++ queststore/controllers/ExperienceLevelsController.java
@@ -0,0 +1,13 @@
+package queststore.controllers;
+
+public class ExperienceLevelsController {
+
+    public void startController() {
+    }
+
+    private void createNewLevel() {
+    }
+
+    private void updateLevel() {
+    }
+}
