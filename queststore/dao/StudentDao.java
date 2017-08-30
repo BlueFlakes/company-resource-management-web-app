15cb4b441b53b1d6ebebdbc093799e99d672f96a
jakubskiba <jakubskibakrk@gmail.com>
2017-08-30 16:35:31 +0200

    mock dao classes

---
+++ queststore/dao/StudentDao.java
@@ -0,0 +1,12 @@
+public class StudentDao{
+    public Object getStudent(Integer id){
+        
+        return new Object();
+    }
+    public Object getStudent(String login){
+        return new Object();
+    }
+    public void save(){
+        
+    }
+}
