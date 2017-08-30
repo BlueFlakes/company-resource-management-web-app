324b0c7f79c319a9d9a74fa2a78d6998c919d011
Kamil <blueflamesxcode@gmail.com>
2017-08-30 17:27:10 +0200

    Add FileLoader class

---
+++ queststore/dao/FileLoader.java
@@ -0,0 +1,31 @@
+package queststore.dao;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileLoader {
+
+    public List<String> getDataFromFile(String fileName) {
+        List<String> temp = new ArrayList<>();
+        Scanner inputStream;
+
+        try {
+            inputStream = new Scanner(new File(fileName));
+
+            while(inputStream.hasNextLine()) {
+                String line = inputStream.nextLine();
+                temp.add(line);
+            }
+
+            inputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return temp;
+    }sdsd
+}
