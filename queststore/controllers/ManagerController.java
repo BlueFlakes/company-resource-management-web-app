31326044f98e7e1e923373c8113922df23741ac3
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-30 14:35:32 +0200

    Add MentorController and ManagerController mock-ups

---
+++ queststore/controllers/ManagerController.java
@@ -0,0 +1,23 @@
+package queststore.controllers;
+
+import queststore.models.User;
+import queststore.models.School;
+
+public class ManagerController {
+
+    public void startController(User user, School school) {
+    }
+
+    public void addMentor() {
+    }
+
+    public void createClass() {
+    }
+
+    public void editMentor() {
+    }
+
+    public void showMentorsClass() {
+    }
+
+}
---
+++ queststore/controllers/MentorController.java
@@ -0,0 +1,26 @@
+package queststore.controllers;
+
+import queststore.models.User;
+import queststore.models.School;
+
+
+public class ManagerController {
+
+    public void startController(User user, School school) {}
+
+    private void addStudent() {
+    }
+
+    private void addQuest() {
+    }
+
+    private void addQuestCategory() {
+    }
+
+    private void updateQuest() {
+    }
+
+    private void markBoughtArtifactsAsUsed() {
+    }
+
+}
