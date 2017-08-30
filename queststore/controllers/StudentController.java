59832b04af1e6393ffa6c7550982ab16d4ece1b7
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-30 14:37:31 +0200

    Add StudentController mock-up

---
+++ queststore/controllers/StudentController.java
@@ -0,0 +1,16 @@
+package queststore.controllers;
+
+import queststore.models.User;
+import queststore.models.School;
+
+public class StudentController {
+
+    public startController(User user, School school) {
+    }
+
+    private void showWallet() {
+    }
+
+    private void showLevel() {
+    }
+}
