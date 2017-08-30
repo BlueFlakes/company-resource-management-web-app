16f5d3382d5cff3ec428f7ca1d63dc6602ad7202
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-30 14:49:55 +0200

    Add Student- & Mentor- StoreControllers mock-ups

---
+++ queststore/controllers/MentorStoreController.java
@@ -0,0 +1,22 @@
+package queststore.controllers;
+
+import queststore.models.School;
+import queststore.models.User;
+
+public StudentStoreController {
+
+    public void startController(User user, School school) {
+    }
+
+    private void showAvailableArtifacts() {
+    }
+
+    private void addArtifact() {
+    }
+
+    private void updateArtifact() {
+    }
+
+    private void addArtifactCategory() {
+    }
+}
---
+++ queststore/controllers/StudentStoreController.java
@@ -0,0 +1,16 @@
+package queststore.controllers;
+
+import queststore.models.School;
+import queststore.models.User;
+
+public StudentStoreController {
+
+    public void startController(User user, School school) {
+    }
+
+    private void buyArtifact() {
+    }
+
+    private void showAvailableArtifacts() {
+    }
+}
