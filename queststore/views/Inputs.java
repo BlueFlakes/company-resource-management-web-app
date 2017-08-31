38977d09c977e123c8381ed470cbe5fd358c11b9
Kamil <blueflamesxcode@gmail.com>
2017-08-31 14:06:02 +0200

    add getMultipleInputs method, which get inputs and delegate their analysis

--- queststore/views/Inputs.java
+++ queststore/views/Inputs.java
@@ -19,3 +19,34 @@
 
         return receivedData;
     }
+
+    private ArrayList<String> getMultipleInputs(String[] deliveredQuestions, String[] expectedTypes) {
+        ArrayList<String> collectedData = new ArrayList<>();
+        boolean isExpectedType;
+        String userInput;
+
+        for(int i = 0; i < deliveredQuestions.length; i++) {
+
+            do {
+                userInput = getInput(deliveredQuestions[i]);
+                isExpectedType = true;
+
+                try {
+                    validateUserInput(userInput, expectedTypes[i]);
+
+                } catch (NumberFormatException e) {
+                    isExpectedType = false;
+                    System.out.println("Error: failure due to wrong input delivered.\n");
+
+                } catch (IllegalArgumentException e) {
+                    isExpectedType = false;
+                    System.out.println(e.getMessage());
+                }
+
+            } while (!isExpectedType);
+
+            collectedData.add(userInput);
+        }
+
+        return collectedData;
+    }
