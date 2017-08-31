3f605e698fe4027aee9db2121b8cf58c103d8dde
Maciej Nowak <mcnowak@wp.pl>
2017-08-31 02:25:52 +0200

    Implement read file in MentorDao

--- queststore/dao/MentorDao.java
+++ queststore/dao/MentorDao.java
@@ -1,22 +1,88 @@
 package queststore.dao;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+import java.io.File;
 import queststore.models.Mentor;
+import queststore.models.Class;
 
-public class MentorDao{
-    public Mentor getMentor(Integer id){
-        
-        return new Mentor("","","","");
+public class MentorDao {
+    private ArrayList<Mentor> mentors;
+
+    public MentorDao(ClassDao classDao) {
+        this.mentors = readMentorsData(classDao);
     }
+
+    private ArrayList<Mentor> readMentorsData(ClassDao classDao) {
+        ArrayList<Mentor> loadedMentors= new ArrayList<>();
+        String[] mentorData;
+        Scanner fileScan;
+
+        try {
+            fileScan = new Scanner(new File("queststore/csv/mentor.csv"));
+
+            while(fileScan.hasNextLine()) {
+                mentorData = fileScan.nextLine().split("\\|");
+                String name = mentorData[1];
+                Integer id = Integer.parseInt(mentorData[0]);
+                String login = mentorData[2];
+                String password = mentorData[3];
+                String email = mentorData[4];
+                Mentor mentor = new Mentor(name, login, password, email, id);
+                loadedMentors.add(mentor);
+
+                Integer classId = Integer.parseInt(mentorData[5]);
+                Class clas = classDao.getClass(classId);
+                clas.addMentor(mentor);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File mentor.csv not found!");
+        }
+
+        return loadedMentors;
+    }
+
+    public Mentor getMentor(Integer id) {
+
+        for(Mentor mentor : this.mentors) {
+
+                if(mentor.getId() == id) {
+                    return mentor;
+                }
+        }
+
+        return null;
+    }
+
     public Mentor getMentor(String login){
-        if (login.equals("skubi")){
-            return new Mentor("","subi","doo","");
+
+        for(Mentor mentor : this.mentors) {
+
+                if(mentor.getLogin().equals(login)) {
+                    return mentor;
+                }
         }
-        else {
-            return null;
-        }
+
+        return null;
     }
 
     public void save(){
-        
+
+    }
+
+    public static void main(String[] args) {
+        ClassDao classDao = new ClassDao();
+        MentorDao mentorDao = new MentorDao(classDao);
+        Mentor mentor = mentorDao.getMentor("sharp");
+        System.out.println(mentor.getName());
+        Class clas = classDao.getClass("klasa druga");
+        System.out.println(clas.getAllMentors().get(2).getName());
+        System.out.println(clas.getAllMentors());
+        Class clas1 = classDao.getClass("klasa pierwsza");
+        System.out.println(clas1.getAllMentors().get(1).getName());
+        //System.out.println(classDao.classes.get(1).getId());
     }
 }
