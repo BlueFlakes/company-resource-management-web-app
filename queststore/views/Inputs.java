7b4c0f2a62a85e157cc3a663901ae0b3667cca1b
Kamil <blueflamesxcode@gmail.com>
2017-08-31 14:04:47 +0200

    add getValidatedInputs method which validate given data and delegate receiving inputs

--- queststore/views/Inputs.java
+++ queststore/views/Inputs.java
@@ -2,3 +2,20 @@
         System.out.print(question + ": ");
         return in.nextLine().trim();
     }
+
+    public ArrayList<String> getValidatedInputs(String[] questions, String[] expectedTypes) {
+        ArrayList<String> receivedData = null;
+
+        try {
+            areEqualByLength(questions, expectedTypes);
+            areDeliveredTypesCorrect(expectedTypes);
+            receivedData = getMultipleInputs(questions, expectedTypes);
+
+        } catch (InvalidArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (NotEqualElementsException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return receivedData;
+    }
