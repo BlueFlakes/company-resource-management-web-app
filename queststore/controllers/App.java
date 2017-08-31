8e3fc48e8d4eae772d646b3696facaf49b031afd
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-31 13:09:37 +0200

    Clean code changes in App.java

--- queststore/controllers/App.java
+++ queststore/controllers/App.java
@@ -3,14 +3,14 @@ package queststore.controllers;
 import queststore.models.School;
 import queststore.exceptions.WrongPasswordException;
 
-public class App{
-    public static void main(String[] args){
+public class App {
+
+    public static void main(String[] args) {
         School school = new School("Codecool");
         try{
             new LoginController(school).start();
-        } catch (WrongPasswordException e){
+        } catch (WrongPasswordException e) {
             System.out.println("No such user");
         }
-
     }
 }
