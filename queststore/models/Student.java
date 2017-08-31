b96dd88ae5619d261ddfd299359f5d75f2c67ff1
JakubJaniszewski <cwanykuba@gmail.com>
2017-08-31 14:34:00 +0200

    Clean code in School, Student and User models

--- queststore/models/Student.java
+++ queststore/models/Student.java
@@ -2,7 +2,8 @@ package queststore.models;
 
 import java.util.ArrayList;
 
-public class Student extends User{
+public class Student extends User {
+
 	private Integer earnedCoins;
 	private Integer possesedCoins;
 	private ArrayList<BoughtArtifact> ownedArtifacts;
@@ -24,32 +25,32 @@ public class Student extends User{
 		this.achievedQuests = new ArrayList<>();
     }
 
-	public Integer getEarnedCoins(){
+	public Integer getEarnedCoins() {
 		return this.earnedCoins;
 	}
 
-	public Integer getPossesedCoins(){
+	public Integer getPossesedCoins() {
 		return this.possesedCoins;
 	}
 
-	public ArrayList<BoughtArtifact> getOwnedArtifacts(){
+	public ArrayList<BoughtArtifact> getOwnedArtifacts() {
 		return this.ownedArtifacts;
 	}
 
-	public ArrayList<Quest> getAchievedQuests(){
+	public ArrayList<Quest> getAchievedQuests() {
 		return this.achievedQuests;
 	}
 
-	public Integer getLevel(ExperienceLevels experienceLevels){
+	public Integer getLevel(ExperienceLevels experienceLevels) {
 		return experienceLevels.computeStudentLevel(this.earnedCoins);
 	}
 
-	public void addCoins(Integer ammount){
+	public void addCoins(Integer ammount) {
 		this.earnedCoins += ammount;
 		this.possesedCoins += ammount;
 	}
 
-	public void substractCoins(Integer ammount){
+	public void substractCoins(Integer ammount) {
 		this.possesedCoins -= ammount;
 	}
 }
