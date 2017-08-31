43bb610638e24e87537135d65fd84e994215309c
jakubskiba <jakubskibakrk@gmail.com>
2017-08-31 14:10:50 +0200

    implement constructors in ExperienceLevels model

--- queststore/models/ExperienceLevels.java
+++ queststore/models/ExperienceLevels.java
@@ -5,6 +5,14 @@ import java.util.TreeMap;
 public class ExperienceLevels{
     private TreeMap<Integer, Integer> levels;
 
+    public ExperienceLevels() {
+        this.levels = new TreeMap<>();
+    }
+
+    public ExperienceLevels(TreeMap<Integer, Integer> levels) {
+        this.levels = levels;
+    }
+
     public Integer computeStudentLevel(Integer coins){
         return (Integer) coins/100;
     }
