06bc002157358cb8ad2d7579b252ccd757256038
Kamil <blueflamesxcode@gmail.com>
2017-08-31 16:54:37 +0200

    Implements mentor store controller architecture, with basic switch between available possibilities.

--- queststore/controllers/MentorStoreController.java
+++ queststore/controllers/MentorStoreController.java
@@ -1,24 +1,75 @@
 package queststore.controllers;
 
 import queststore.interfaces.UserController;
+import queststore.views.UserInterface;
 
 import queststore.models.School;
 import queststore.models.User;
 
 public class MentorStoreController implements UserController {
+    private UserInterface userInterface = new UserInterface();
+    private User user;
+    private School school;
 
     public void startController(User user, School school) {
+
+        this.user = user;
+        this.school = school;
+        String userChoice = "";
+
+        while (!userChoice.equals("0")) {
+            this.userInterface.printMentorMenu();
+            userChoice = userInterface.getInput("What do you want to do");
+            handleUserRequest(userChoice);
+            userInterface.lockActualState();
+            school.save();
+        }
+    }
+
+    private void handleUserRequest(String userChoice) {
+
+        switch(userChoice) {
+            case "1":
+                showAvailableArtifacts();
+                break;
+
+            case "2":
+                addArtifact();
+                break;
+
+            case "3":
+                updateArtifact();
+                break;
+
+            case "4":
+                addArtifactCategory();
+                break;
+
+            case "0":
+                break;
+
+            default:
+                handleNoSuchCommand();
+        }
     }
 
     private void showAvailableArtifacts() {
+        System.out.println("Available Artifacts.");
     }
 
     private void addArtifact() {
+        System.out.println("Add artifact.");
     }
 
     private void updateArtifact() {
+        System.out.println("update artifact");
     }
 
     private void addArtifactCategory() {
+        System.out.println("Add artifact category");
+    }
+
+    private void handleNoSuchCommand() {
+        userInterface.print("Wrong command!");
     }
 }
