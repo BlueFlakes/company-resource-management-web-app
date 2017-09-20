b35fb2cb478775f36208780bd3b8a18f6db4a4c0
Maciej Nowak <mcnowak@wp.pl>
2017-09-20 23:38:30 +0200

    Implement DatabaseConnection class

---
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/dao/DatabaseConnection.java
@@ -0,0 +1,30 @@
+package com.codecool.krk.lucidmotors.queststore.dao;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null) {
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection("jdbc:sqlite:data/Codecool.db");
+            }
+        } catch (SQLException e) {
+            System.out.println("Database connection failure!");
+        } catch (ClassNotFoundException e1) {
+            System.out.println(e1.getMessage());
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Database connection closing failure!");
+        }
+
+    }
+}
