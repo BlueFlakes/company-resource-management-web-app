a7789be17294ef43389f447dbe44132f933d7765
jakubskiba <jakubskibakrk@gmail.com>
2017-09-20 22:02:45 +0200

    implement constructors

--- src/main/java/com/codecool/krk/lucidmotors/queststore/models/BoughtArtifact.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/models/BoughtArtifact.java
@@ -8,12 +8,16 @@ public class BoughtArtifact extends AbstractArtifact {
     private Date date;
     private boolean isUsed;
 
-    public ArrayList<Student> getOwnersList() {
-        return this.ownersList;
+    public BoughtArtifact(ShopArtifact shopArtifact) {
+        super(shopArtifact.getName(), shopArtifact.getPrice(), shopArtifact.getArtifactCategory(), shopArtifact.getDescription(), null);
+        this.date = new Date();
+        this.isUsed = false;
     }
 
-    public boolean getIsUsed() {
-        return this.isUsed;
+    public BoughtArtifact(String name, Integer price, ArtifactCategory artifactCategory, String description, Integer id, Date date, boolean isUsed) {
+        super(name, price, artifactCategory, description, id);
+        this.date = date;
+        this.isUsed = isUsed;
     }
 
     public void markAsUsed() {
