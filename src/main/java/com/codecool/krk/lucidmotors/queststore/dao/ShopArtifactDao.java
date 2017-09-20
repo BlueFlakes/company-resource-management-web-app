4ce3954eff0bc00e76492ad016e002bda9f01846
jakubskiba <jakubskibakrk@gmail.com>
2017-09-20 22:00:05 +0200

    mock shop artifact dao

--- src/main/java/com/codecool/krk/lucidmotors/queststore/dao/ShopArtifactDao.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/dao/ShopArtifactDao.java
@@ -1,4 +1,36 @@
 package com.codecool.krk.lucidmotors.queststore.dao;
 
+import com.codecool.krk.lucidmotors.queststore.models.ArtifactCategory;
+import com.codecool.krk.lucidmotors.queststore.models.ShopArtifact;
+
+import java.util.ArrayList;
+
 public class ShopArtifactDao {
+    public ShopArtifact getArtifact(Integer id){
+        /* #TODO database request*/
+        if(id.equals(10)) {
+            return new ShopArtifact("Combat training", 50, new ArtifactCategory(), "Private mentoring", 10);
+        } else if(id.equals(20)) {
+            return new ShopArtifact("Temple", 500, new ArtifactCategory(), "Private mentoring", 20);
+        } else {
+            return null;
+        }
+
+    }
+
+    public void updateArtifact(ShopArtifact shopArtifact) {
+        /* #TODO database request*/
+
+    }
+
+    public ArrayList<ShopArtifact> getAllArtifacts() {
+        /* #TODO database request*/
+        ArrayList<ShopArtifact> list = new ArrayList<>();
+        list.add(new ShopArtifact("Combat training", 50, new ArtifactCategory(), "Private mentoring", 10));
+        list.add(new ShopArtifact("Combat training", 50, new ArtifactCategory(), "Private mentoring", 20));
+        return list;
+    }
+
+
+
 }
