835aefc249d467e0ca5737d94a74265d3150a963
Kamil <blueflamesxcode@gmail.com>
2017-09-21 18:00:51 +0200

    refactor clean code

--- src/main/java/com/codecool/krk/lucidmotors/queststore/controllers/ExperienceLevelsController.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/controllers/ExperienceLevelsController.java
@@ -1,27 +1,27 @@
 package com.codecool.krk.lucidmotors.queststore.controllers;
 
-import com.codecool.krk.lucidmotors.queststore.interfaces.UserController;
+import java.util.ArrayList;
 
+import com.codecool.krk.lucidmotors.queststore.interfaces.UserController;
 import com.codecool.krk.lucidmotors.queststore.models.User;
 import com.codecool.krk.lucidmotors.queststore.models.School;
-
 import com.codecool.krk.lucidmotors.queststore.views.UserInterface;
 
-import java.util.ArrayList;
 
 public class ExperienceLevelsController implements UserController {
 
-    User user;
-    School school;
-    UserInterface userInterface = new UserInterface();
+    private User user;
+    private School school;
+    private final UserInterface userInterface = new UserInterface();
 
     public void startController(User user, School school) {
 
         this.user = user;
         this.school = school;
-
         String userChoice = "";
+
         while(!userChoice.equals("0")) {
+
             this.userInterface.printExperienceLevelsMenu();
             userChoice = this.userInterface.inputs.getInput("Provide options: ");
             handleUserRequest(userChoice);
@@ -33,6 +33,7 @@ public class ExperienceLevelsController implements UserController {
     private void handleUserRequest(String choice) {
 
         switch(choice) {
+
             case "1":
                 createNewLevel();
                 break;
@@ -51,6 +52,7 @@ public class ExperienceLevelsController implements UserController {
     }
 
     private void createNewLevel() {
+
         String[] questions = {"level: ", "needed coins: "};
         String[] types = {"integer", "integer"};
         ArrayList<String> answers = this.userInterface.inputs.getValidatedInputs(questions, types);
@@ -60,6 +62,7 @@ public class ExperienceLevelsController implements UserController {
     }
 
     private void updateLevel() {
+
         String[] questions = {"level: ", "needed coins: "};
         String[] types = {"integer", "integer"};
         ArrayList<String> answers = this.userInterface.inputs.getValidatedInputs(questions, types);
