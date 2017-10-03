40a2f63f2de0f6131b5291a66d86ae87b6e549f7
Kamil <blueflamesxcode@gmail.com>
2017-10-03 16:55:50 +0200

    change menu switch case inside StudentController now use enum

---
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/enums/StudentControllerMenuOptions.java
@@ -0,0 +1,9 @@
+package com.codecool.krk.lucidmotors.queststore.enums;
+
+public enum StudentControllerMenuOptions {
+    LOGOUT,
+    START_STORE_CONTROLLER,
+    SHOW_LEVEL,
+    SHOW_WALLET,
+    DEFAULT
+}
