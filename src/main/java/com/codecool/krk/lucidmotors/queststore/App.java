87ede20d9691b47e822e076b7f3c28bae9999d32
jakubskiba <jakubskibakrk@gmail.com>
2017-11-14 01:24:28 +0100

    change architecture to fit web

--- src/main/java/com/codecool/krk/lucidmotors/queststore/App.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/App.java
@@ -1,10 +1,13 @@
 package com.codecool.krk.lucidmotors.queststore;
 
 import com.codecool.krk.lucidmotors.queststore.controllers.LoginController;
+import com.codecool.krk.lucidmotors.queststore.controllers.MainController;
 import com.codecool.krk.lucidmotors.queststore.exceptions.DaoException;
 import com.codecool.krk.lucidmotors.queststore.models.School;
 import com.codecool.krk.lucidmotors.queststore.exceptions.WrongPasswordException;
 import com.codecool.krk.lucidmotors.queststore.dao.DatabaseConnection;
+
+import java.io.IOException;
 
 class App {
 
@@ -12,19 +15,13 @@ class App {
 
         try {
             School school = new School("Codecool");
-            new LoginController(school).start();
+            new MainController(school).start();
 
         } catch (DaoException ex) {
             System.out.println("Database connection failed!\n" + ex.getMessage());
 
-        } finally {
-
-            try {
-                DatabaseConnection.closeConnection();
-            } catch (DaoException ex) {
-                System.out.println("Database connection failed!\n" + ex.getMessage());
-            }
-
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
     }
---
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/controllers/MainController.java
@@ -0,0 +1,35 @@
+package com.codecool.krk.lucidmotors.queststore.controllers;
+import com.codecool.krk.lucidmotors.queststore.handlers.*;
+import com.codecool.krk.lucidmotors.queststore.models.School;
+import com.codecool.krk.lucidmotors.queststore.models.User;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MainController {
+
+    School school;
+
+    public MainController(School school) {
+        this.school = school;
+    }
+
+    private static Integer port = 8000;
+
+    public void start() throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        Map<UUID, User> loggedUsers = new HashMap<>();
+
+        server.createContext("/", new LoginHandler(this.school, loggedUsers));
+        server.createContext("/static", new Static());
+        server.setExecutor(null); // creates a default executor
+
+        server.start();
+    }
+}
