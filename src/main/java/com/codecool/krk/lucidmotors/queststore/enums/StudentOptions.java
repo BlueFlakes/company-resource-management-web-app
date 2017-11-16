a72181254088293d54a0e7ad7ce4820b02186cfa
Maciej Nowak <mcnowak@wp.pl>
2017-11-16 11:20:13 +0100

    Add Student enum and menu

--- src/main/java/com/codecool/krk/lucidmotors/queststore/enums/StudentOptions.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/enums/StudentOptions.java
@@ -1,9 +1,22 @@
 package com.codecool.krk.lucidmotors.queststore.enums;
 
 public enum StudentOptions {
-    START_STORE_CONTROLLER,
-    SHOW_LEVEL,
-    SHOW_WALLET,
-    SHOW_AVAILABLE_ARTIFACTS,
-    BUY_ARTIFACT
+    STORE ("templates/empty.twig"),
+    SHOW_WALLET ("templates/empty.twig"),
+    SHOW_AVAILABLE_ARTIFACTS ("templates/empty.twig"),
+    BUY_ARTIFACT ("templates/empty.twig"),
+    CONTRIBUTIONS ("templates/empty.twig"),
+    CREATE_CONTRIBUTION ("templates/empty.twig"),
+    CLOSE_CONTRIBUTION ("templates/empty.twig"),
+    DEFAULT ("templates/empty.twig");
+
+    private String path;
+
+    private StudentOptions(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
 }
