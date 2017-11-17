9403a2378a7cfd54a9b18134e00cca8c34648430
jakubskiba <jakubskibakrk@gmail.com>
2017-11-17 13:42:19 +0100

    create student view

---
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/views/StudentView.java
@@ -0,0 +1,44 @@
+package com.codecool.krk.lucidmotors.queststore.views;
+
+import com.codecool.krk.lucidmotors.queststore.enums.MentorOptions;
+import com.codecool.krk.lucidmotors.queststore.exceptions.DaoException;
+import com.codecool.krk.lucidmotors.queststore.models.Activity;
+import com.codecool.krk.lucidmotors.queststore.models.School;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+public class StudentView {
+    School school;
+    public StudentView(School school) {
+        this.school = school;
+    }
+
+    public Activity getActivity(MentorOptions mentorOption) {
+        String response;
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("/templates/main.twig");
+        JtwigModel model = JtwigModel.newModel();
+
+        model.with("title", mentorOption.toString());
+        model.with("menu_path", "classpath:/templates/snippets/mentor-menu-snippet.twig");
+        try {
+            insertData(mentorOption, model);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
+
+        String contentPath = "classpath:/" + mentorOption.getPath();
+        model.with("content_path", contentPath);
+
+        response = template.render(model);
+
+        return new Activity(200, response);
+    }
+
+    private void insertData(MentorOptions mentorOption, JtwigModel model) throws DaoException {
+        switch (mentorOption) {
+//            case SHOW_MENTORS_CLASS:
+//                model.with("mentors", this.school.getAllMentors());
+//                break;
+        }
+    }
+}
