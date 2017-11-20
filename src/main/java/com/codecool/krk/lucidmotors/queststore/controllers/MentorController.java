5c4233e2e79a780c3773126ca6f0e28c25e0cd3f
Maciej Nowak <mcnowak@wp.pl>
2017-11-20 20:48:33 +0100

    Start implementation of MentorController class

--- src/main/java/com/codecool/krk/lucidmotors/queststore/controllers/MentorController.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/controllers/MentorController.java
@@ -416,3 +416,99 @@
 //        this.userInterface.pause();
 //    }
 //}
+
+package com.codecool.krk.lucidmotors.queststore.controllers;
+
+import com.codecool.krk.lucidmotors.queststore.dao.*;
+import com.codecool.krk.lucidmotors.queststore.exceptions.DaoException;
+import com.codecool.krk.lucidmotors.queststore.models.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class MentorController {
+    private QuestCategoryDao questCategoryDao = new QuestCategoryDao();
+    private AvailableQuestDao availableQuestDao = new AvailableQuestDao();
+    School school;
+
+    public MentorController(School school) throws DaoException {
+        this.school = school;
+    }
+
+    public boolean addArtifactCategory(Map<String, String> formData) throws DaoException {
+        String name = formData.get("name");
+
+        ArtifactCategory artifactCategory = new ArtifactCategory(name);
+        new ArtifactCategoryDao().save(artifactCategory);
+
+        return true;
+    }
+
+    public boolean addStudent(Map<String, String> formData) throws DaoException {
+        Boolean isAdded = false;
+
+        if (this.school.isLoginAvailable(formData.get("login"))) {
+            String name = formData.get("name");
+            String login = formData.get("login");
+            String password = formData.get("password");
+            String email = formData.get("email");
+            Integer classId = Integer.valueOf(formData.get("class_id"));
+            SchoolClass chosenClass = chooseProperClass(classId);
+            Student student = new Student(name, login, password, email, chosenClass);
+            student.save();
+            isAdded = true;
+        }
+
+        return isAdded;
+    }
+
+    private SchoolClass chooseProperClass(Integer classId) throws DaoException {
+
+        SchoolClass schoolClass = new ClassDao().getSchoolClass(classId);
+
+        return schoolClass;
+    }
+
+//
+//    public List<Student> getMentorClass(Integer mentorId) throws DaoException {
+//        Mentor mentor = school.getMentor(mentorId);
+//
+//        if (mentor == null) {
+//            return null;
+//        } else {
+//            return mentor.getClas().getAllStudents();
+//        }
+//    }
+//
+//    public boolean editMentor(Map<String, String> formData) throws DaoException {
+//        Boolean isUpdated = false;
+//
+//        Integer mentorId = Integer.valueOf(formData.get("mentor_id"));
+//        Mentor mentor = new MentorDao(new ClassDao()).getMentor(mentorId);
+//
+//        if (mentor != null &&
+//                (this.school.isLoginAvailable(formData.get("login")) || formData.get("login").equals(mentor.getLogin()))) {
+//            mentor.setName(formData.get("name"));
+//            mentor.setLogin(formData.get("login"));
+//            mentor.setPassword(formData.get("password"));
+//            mentor.setEmail(formData.get("mail"));
+//            mentor.update();
+//            isUpdated = true;
+//        }
+//
+//        return isUpdated;
+//    }
+//
+
+//
+
+//
+//    public boolean createClass(Map<String, String> formData) throws DaoException {
+//        String name = formData.get("classname");
+//
+//        SchoolClass schoolClass = new SchoolClass(name);
+//        schoolClass.save();
+//
+//        return true;
+//    }
+}
