de2456e23cb3adbfb5592d070902ee06e261f623
Kamil <blueflamesxcode@gmail.com>
2017-11-20 12:56:49 +0100

    delete wrong imports

--- src/main/java/com/codecool/krk/lucidmotors/queststore/App.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/App.java
@@ -4,9 +4,7 @@ import com.codecool.krk.lucidmotors.queststore.controllers.MainController;
 import com.codecool.krk.lucidmotors.queststore.dao.DatabaseConnection;
 import com.codecool.krk.lucidmotors.queststore.exceptions.DaoException;
 import com.codecool.krk.lucidmotors.queststore.models.School;
-import com.sun.org.apache.xpath.internal.operations.Bool;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.util.Scanner;
 
