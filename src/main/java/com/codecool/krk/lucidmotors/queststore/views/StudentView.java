012c437cff7e1108deea4688fa8361e68e648c22
Kamil <blueflamesxcode@gmail.com>
2017-11-21 11:24:54 +0100

    extract cases to methods and change method call order

--- src/main/java/com/codecool/krk/lucidmotors/queststore/views/StudentView.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/views/StudentView.java
@@ -22,16 +22,10 @@ public class StudentView {
     }
 
     public Activity getActivity(StudentOptions studentOption) throws DaoException{
-        System.out.println(studentOption);
+
         String response;
         JtwigTemplate template = JtwigTemplate.classpathTemplate("/templates/main.twig");
         JtwigModel model = JtwigModel.newModel();
-
-        model.with("user", user);
-        model.with("role", user.getClass().getSimpleName());
-        model.with("level", this.studentController.getUserLevel(user));
-        model.with("title", studentOption.toString());
-        model.with("menu_path", "classpath:/templates/snippets/student-menu-snippet.twig");
 
         try {
             insertData(studentOption, model);
@@ -39,6 +33,14 @@ public class StudentView {
         } catch (DaoException e) {
             e.printStackTrace();
         }
+
+        Student student = this.studentController.getStudent(user.getId());
+
+        model.with("user", student);
+        model.with("role", student.getClass().getSimpleName());
+        model.with("level", this.studentController.getUserLevel(student));
+        model.with("title", studentOption.toString());
+        model.with("menu_path", "classpath:/templates/snippets/student-menu-snippet.twig");
 
         String contentPath = "classpath:/" + studentOption.getPath();
         model.with("content_path", contentPath);
@@ -51,77 +53,98 @@ public class StudentView {
     private void insertData(StudentOptions studentOption, JtwigModel model) throws DaoException {
         switch (studentOption) {
             case SHOW_WALLET:
-                List<BoughtArtifact> boughtArtifacts = studentController.getWallet(this.user);
-                model.with("bought_artifacts", boughtArtifacts);
+                showWallet(model);
                 break;
 
             case SHOW_AVAILABLE_ARTIFACTS:
-                List<ShopArtifact> shopArtifacts = studentController.getShopArtifacts();
-                model.with("shop_artifacts", shopArtifacts);
+                showAvailableArtifacts(model);
                 break;
 
             case BUY_ARTIFACT:
-
-                if (formData.containsKey("choosen-artifact")) {
-                    boolean wasSuccesfullyBought = studentController.buyArtifact(this.formData, this.user);
-                    model.with("is_text_available", true);
-
-                    String message;
-                    if (wasSuccesfullyBought) {
-                        message = "Artifact succesfuly bought!";
-                    } else {
-                        message = "Sorry but you dont have enough money!";
-                    }
-
-                    model.with("text", message);
-
-                } else {
-                    List<ShopArtifact> shopArtifacts1 = studentController.getShopArtifacts();
-                    model.with("shop_artifact", shopArtifacts1);
-                }
+                handleArtifactShopping(model);
                 break;
 
             case AVAILABLE_CONTRIBUTIONS:
-                List<Contribution> contributions = studentController.getAvailableContributions();
-                model.with("available_contributions", contributions);
-                boolean succesfullyPaid = studentController.takePartInContribution(formData, user);
-
-
-                String message;
-                if (succesfullyPaid) {
-                    model.with("is_text_available", true);
-                    message = "Succesfuly Paid! :)";
-
-                } else {
-                    message = "Sorry but you dont have enough money!";
-                }
-
-                model.with("text", message);
+                handleJoinContribution(model);
                 break;
 
             case CREATE_CONTRIBUTION:
-                List<ShopArtifact> shopArtifacts1 = studentController.getShopArtifacts();
-                model.with("shop_artifact", shopArtifacts1);
-                boolean wasSuccesfullyAdded = studentController.addNewContribution(formData, user);
-
-                if (wasSuccesfullyAdded) {
-                    model.with("is_text_available", true);
-                    model.with("text", "Succesfully added contribution!");
-                }
+                handleCreateContribution(model);
                 break;
 
             case CLOSE_CONTRIBUTION:
-                List<Contribution> userContributions = studentController.getAllAuthorContributions(user);
-                model.with("user_contributions", userContributions);
-                boolean wasSuccesfulyClosed = studentController.closeUserContribution(formData, user);
-
-                if (wasSuccesfulyClosed) {
-                    model.with("is_text_available", true);
-                    model.with("text", "Succesfully closed contribution!");
-
-                }
-
+                handleCloseContribution(model);
                 break;
+        }
+    }
+
+    private void showWallet(JtwigModel model) throws DaoException {
+        List<BoughtArtifact> boughtArtifacts = studentController.getWallet(this.user);
+        model.with("bought_artifacts", boughtArtifacts);
+    }
+
+    private void showAvailableArtifacts(JtwigModel model) throws DaoException {
+        List<ShopArtifact> shopArtifacts = studentController.getShopArtifacts();
+        model.with("shop_artifacts", shopArtifacts);
+    }
+
+    private void handleArtifactShopping(JtwigModel model) throws DaoException {
+        if (formData.containsKey("choosen-artifact")) {
+            boolean wasSuccesfullyBought = studentController.buyArtifact(this.formData, this.user);
+            model.with("is_text_available", true);
+
+            String message;
+            if (wasSuccesfullyBought) {
+                message = "Artifact succesfuly bought!";
+            } else {
+                message = "Sorry but you dont have enough money!";
+            }
+
+            model.with("text", message);
+
+        } else {
+            List<ShopArtifact> shopArtifacts1 = studentController.getShopArtifacts();
+            model.with("shop_artifact", shopArtifacts1);
+        }
+    }
+
+    private void handleJoinContribution(JtwigModel model) throws DaoException {
+        List<Contribution> contributions = studentController.getAvailableContributions();
+        model.with("available_contributions", contributions);
+        boolean succesfullyPaid = studentController.takePartInContribution(formData, user);
+
+        String message;
+        if (succesfullyPaid) {
+            model.with("is_text_available", true);
+            message = "Succesfuly Paid! :)";
+
+        } else {
+            message = "Sorry but you dont have enough money!";
+        }
+
+        model.with("text", message);
+    }
+
+    private void handleCreateContribution(JtwigModel model) throws DaoException {
+        List<ShopArtifact> shopArtifacts1 = studentController.getShopArtifacts();
+        model.with("shop_artifact", shopArtifacts1);
+        boolean wasSuccesfullyAdded = studentController.addNewContribution(formData, user);
+
+        if (wasSuccesfullyAdded) {
+            model.with("is_text_available", true);
+            model.with("text", "Succesfully added contribution!");
+        }
+    }
+
+    private void handleCloseContribution(JtwigModel model) throws DaoException {
+        List<Contribution> userContributions = studentController.getAllAuthorContributions(user);
+        model.with("user_contributions", userContributions);
+        boolean wasSuccesfulyClosed = studentController.closeUserContribution(formData, user);
+
+        if (wasSuccesfulyClosed) {
+            model.with("is_text_available", true);
+            model.with("text", "Succesfully closed contribution!");
+
         }
     }
 }
