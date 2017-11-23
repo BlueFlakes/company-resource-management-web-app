7b21d5ec662d14f73cef6c2e911acd2d69c5d059
jakubskiba <jakubskibakrk@gmail.com>
2017-11-23 16:52:15 +0100

    remove redundant files and lines from controllers

--- src/main/java/com/codecool/krk/lucidmotors/queststore/controllers/LoginController.java
+++ src/main/java/com/codecool/krk/lucidmotors/queststore/controllers/LoginController.java
@@ -6,72 +6,12 @@ import com.codecool.krk.lucidmotors.queststore.models.*;
 
 public class LoginController {
 
-    //private final UserInterface userInterface = new UserInterface();
-    //private final LoginView loginView = new LoginView();
     private School school;
 
     public LoginController(School school) {
         this.school = school;
     }
 
-    /**
-     * Gets login and password from user, check existance of such user.
-     * If password is correct runs appropriate controller.
-     *
-     * @throws WrongPasswordException
-     * @throws DaoException
-     */
-    public void start() throws DaoException {
-
-        String userChoice;
-//
-//        do {
-//            showMenu();
-//            //userChoice = //userInterface.inputs.getInput("What do you want to do: ");
-//            //userInterface.clearWindow();
-//            //handleUserRequest(userChoice);
-//
-//        } while (!userChoice.equals("0"));
-    }
-
-//    private void handleUserRequest(String userChoice) throws DaoException {
-//
-//        //LoginMenuOptions chosenOption = null;
-//
-//        switch (chosenOption) {
-//            case HANDLE_LOGIN:
-//                //
-//                break;
-//
-//            case EXIT:
-//                //userInterface.println("Have a nice day!");
-//                break;
-//
-//            case DEFAULT:
-//                //handleNoSuchCommand();
-//        }
-//    }
-
-//    private void handleNoSuchCommand() {
-////        userInterface.println("Wrong choice");
-////        userInterface.pause();
-//    }
-
-//    private LoginMenuOptions getEnumValue(String userChoice) {
-//        LoginMenuOptions chosenOption = null;
-//
-//        try {
-//            //chosenOption = LoginMenuOptions.values()[Integer.parseInt(userChoice)];
-//        } catch (IndexOutOfBoundsException | NumberFormatException e) {
-//            //chosenOption = LoginMenuOptions.DEFAULT;
-//        }
-//
-//        return chosenOption;
-//    }
-
-    private void showMenu() {
-        //this.loginView.printLoginMenu();
-    }
 
     public User getUser(String login, String password) throws DaoException {
         User user = this.school.getUser(login, password);
@@ -79,22 +19,4 @@ public class LoginController {
         return user;
     }
 
-    /**
-     * Runs appropriate controller based on user Type
-     *
-     * @param user
-     * @throws DaoException
-     */
-    private void runUserController(User user) throws DaoException {
-
-        if (user instanceof Manager) {
-            //new ManagerController().startController(user, this.school);
-
-        } else if (user instanceof Mentor) {
-            //new MentorController().startController(user, this.school);
-
-        } else if (user instanceof Student) {
-            //new StudentController().startController(user, this.school);
-        }
-    }
 }
